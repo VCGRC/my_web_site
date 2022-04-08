b027a5e2814e104b4c1f1cd1d1a067eded934e75
VCGRC <fomablinov2015.cool@ya.ru>
2022-04-09 00:35:57 +0300

    fix

--- bot_api/models.py
+++ bot_api/models.py
@@ -11,6 +11,6 @@ ipc_client = ipc.Client(secret_key=app.secret_key)
 class Bot:
 
     async def ping(self) -> float:
-        ping = await ipc_client.request('ping')
+        ping = await ipc_client.request('get_ping')
 
         return str(ping) 
