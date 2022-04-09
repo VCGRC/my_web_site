5a2df8669c3846161b9beb34185f44b1dcf1ed9f
VCGRC <fomablinov2015.cool@ya.ru>
2022-04-09 13:36:59 +0300

    fix

--- decorators.py
+++ decorators.py
@@ -2,9 +2,9 @@ from functools import wraps
 from flask import Flask, redirect, render_template, session, request, url_for
 from app import app
 
-def login_required(f):
+async def login_required(f):
     @wraps(f)
-    def wrap(*args, **kwargs):
+    async def wrap(*args, **kwargs):
         if 'logged_in' in session:
             return f(*args, **kwargs)
         else:
@@ -12,10 +12,10 @@ def login_required(f):
 
     return wrap
 
-def access_level(level = 0):
-    def decorator(f):
+async def access_level(level = 0):
+    async def decorator(f):
         @wraps(f)
-        def wrap(*args, **kwargs):
+        async def wrap(*args, **kwargs):
             if 'logged_in' in session:
                 if session['user']['access_level']>=level:
                     return f(*args, **kwargs)
