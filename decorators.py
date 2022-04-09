f46b5772aaa333002c95c1a496bb32aec3fe3901
VCGRC <fomablinov2015.cool@ya.ru>
2022-04-09 15:00:19 +0300

    fix

--- decorators.py
+++ decorators.py
@@ -1,28 +1,27 @@
 from functools import wraps
-from flask import Flask, redirect, render_template, session, request, url_for
+from quart import Quart, redirect, render_template, session, request, url_for
 from app import app
 # from quart_auth import login_required
 
-def login_required(fn):
-    @wraps(fn)
-    async def wrapper(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect('/')
-        else:
-            return await fn(*args, **kwargs)
-
-    return wrapper
-
-# def login_required(f):
-#     @wraps(f)
-#     async def wrap(*args, **kwargs):
-#         session = await session
-#         if 'logged_in' in session:
-#             return await f(*args, **kwargs)
+# def login_required(fn):
+#     @wraps(fn)
+#     async def wrapper(*args, **kwargs):
+#         if session.get("user_id") is None:
+#             return redirect('/')
 #         else:
-#             return await redirect('/')
+#             return await fn(*args, **kwargs)
 
-#     return wrap
+#     return wrapper
+
+def login_required(f):
+    @wraps(f)
+    async def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return await f(*args, **kwargs)
+        else:
+            return await redirect('/')
+
+    return wrap
 
 def access_level(level = 0):
     def decorator(f):
