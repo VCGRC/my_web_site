d02dff18b241d3a1189b78216fdc28a3c37b06bc
VCGRC <fomablinov2015.cool@ya.ru>
2022-04-21 01:05:44 +0300

    fix

--- bot_api/routes.py
+++ bot_api/routes.py
@@ -1,13 +1,13 @@
 from functools import wraps
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app import app
 from bot_api.models import Bot
 from decorators import login_required, access_level
 import pdb
 
-@access_level(level=5)
+
 @app.route('/status/', methods = ['GET'])
-async def status():
+async def status(request:Request):
     # pdb.set_trace()
     ping = await Bot().ping()
     # return ping
