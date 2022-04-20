712a58683da6328d932b348cf6401b5d13935515
VCGRC <fomablinov2015.cool@ya.ru>
2022-04-20 23:57:56 +0300

    fix

--- app.py
+++ app.py
@@ -42,8 +42,7 @@ async def create_news():
 async def get():
     news_collection = cluster.web.news
     news = news_collection.find().sort('create_date', pymongo.DESCENDING).limit(10)
-    print(list(news))
-    return jsonify(list(news))
+    return jsonify([x for x in news])
 
 from user.routes import *
 from bot_api.routes import *
