81833c94ebb4bc28d8f929bb16e470882a1df94e
VCGRC <fomablinov2015.cool@ya.ru>
2022-04-21 11:29:25 +0300

    fix

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 import datetime
 import uvicorn
+from pydantic import BaseModel
 load_dotenv(find_dotenv())
 
 app = FastAPI()
@@ -24,29 +25,39 @@ app.add_middleware(
 # cluster = pymongo.MongoClient(os.environ.get('MONGO_URI'))
 cluster = MongoClient('localhost')
 
-# @app.route('/', methods = ['GET'])
-# async def index():
-#     news_collection = cluster.web.news
-#     news = news_collection.find().sort('create_date', pymongo.DESCENDING).limit(10)
-#     return await render_template('index.html', news = news)
+class Article(BaseModel):
+    text: str
+    title: str
 
-# @app.route('/create_news/', methods = ['GET', 'POST'])
-# async def create_news():
-#     if request.method == 'GET':
-#         return await render_template('create_news.html')
+@app.post('/api/v1/news/create')
+async def create_new(article:Article):
+    news_collection = cluster.web.news
+    record = {
+        'create_date':datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+        'text':article.text,
+        'title':article.title
+    }
+    try:
+        res = news_collection.insert_one(record)
+        return {'result':'success', '_id':str(res.inserted_id)}
+    except Exception as e:
+        return {'result':'error', 'error':str(e)}
 
-#     if request.method == 'POST':
+@app.delete('/api/v1/news/delete/<_id>')
+async def delete_new(_id, request:Request):
+    news_collection = cluster.web.news
+    try:
+        new = news_collection.delete_one({'_id':_id})
+        return {'result':'success'}
+    except Exception as e:
+        return {'result':'error', 'error':str(e)}
 
-#         news_collection = cluster.web.news
-#         data = await request.form
-#         record = {
-#             'create_date':datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-#             'text':data['text'],
-#             'title':data['title']
-#         }
-
-#         news_collection.insert_one(record)
-#         return redirect('/')
+@app.get('/api/v1/news/get/<_id>')
+async def get_one_new(_id, request:Request):
+    news_collection = cluster.web.news
+    new = news_collection.find_one({'_id':_id})
+    data = {'title':new['title'], 'create_date':new['create_date'], 'text':new['text'], '_id':str(new['_id'])}
+    return data
     
 @app.get('/api/v1/news/get')
 async def get_news(request:Request):
